2f6450ca9b58b0889947c50c64eb37c870a346b4
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2022-11-29 14:44:53 -0300

    Paginas foram dinamizadas

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  photo: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  description: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
